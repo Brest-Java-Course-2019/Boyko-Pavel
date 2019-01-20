1321624862933998f18f613a8e39ee845636b668
Pavel Boyko <ipave1boykoi@gmail.com>
2019-01-20 15:50:15 +0300

    count delivery from json

--- src/main/java/com/epam/brest/course/DeliveryCost.java
+++ src/main/java/com/epam/brest/course/DeliveryCost.java
@@ -1,36 +1,66 @@
 package com.epam.brest.course;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class DeliveryCost {
 
+    private double weight;
+    private double distans;
+    private Scanner scan;
+    private double koef;
+
+    {
+        scan = new Scanner(System.in);
+    }
+
+    public DeliveryCost() {
+        this.weight = scan("Enter weight");
+        this.distans = scan("Enter distans");
+        setKoef();
+    }
+
     public void costDelivey() {
 
-        long val = scanDistanse() * scanWeight() * 4;
-        System.out.printf("Cost delivery: %d $", val);
-
+        double value = this.distans * this.weight * this.koef;
+        System.out.printf("Cost delivery: %.3f $", value);
     }
 
-    public long scanWeight() {
-        System.out.println("Enter weigh");
-        Scanner scan = new Scanner(System.in);
-        while (!scan.hasNextLong()) {
-            System.out.println("Enter correct weigh");
-            scan = new Scanner(System.in);
+    public double scan(String text) {
 
+        System.out.println(text);
+        while (!this.scan.hasNextDouble()) {
+            System.out.println("Enter correct value");
+            this.scan.nextLine();
         }
-        return scan.nextLong();
+        return this.scan.nextDouble();
     }
 
-    public long scanDistanse() {
+    public void setKoef() {
 
-        System.out.println("Enter  dictance");
-        Scanner scan = new Scanner(System.in);
-        while (!scan.hasNextLong()) {
-            System.out.println("Enter correct dictance");
-            scan = new Scanner(System.in);
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader("koef.json"));
+            JSONObject jsonObject = (JSONObject) obj;
+            JSONArray jsonArray = (JSONArray) jsonObject.get("koefs");
+            for (Object koefItem : jsonArray) {
+                JSONObject item = (JSONObject) koefItem;
+                Double start = (Double) item.get("start");
+                Double end = (Double) item.get("end");
+                Double koef = (Double) item.get("koef");
+                if (this.weight >= start & this.weight < end) {
+                    this.koef = koef;
+                }
+            }
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
         }
-        return scan.nextLong();
     }
 
 }
--- src/main/java/com/epam/brest/course/Main.java
+++ src/main/java/com/epam/brest/course/Main.java
@@ -1,8 +1,11 @@
 package com.epam.brest.course;
 
+
+import java.math.BigDecimal;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         DeliveryCost del = new DeliveryCost();
         del.costDelivey();
     }
