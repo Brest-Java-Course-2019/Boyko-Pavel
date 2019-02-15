18910ed899bfef55854837cf442e496e322df6cc
pavel <http://www.iam.fmph.uniba.sk/amuc/ojs/index.php/amuc/article/view/430>
2019-02-15 15:33:18 +0300

    del .idea

--- StudentTesting/test-db/src/main/resources/create-tables.sql
+++ StudentTesting/test-db/src/main/resources/create-tables.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS subject;
 CREATE TABLE subject (
-  id_subject INT NOT NULL,
+  id_subject INT NOT NULL AUTO_INCREMENT,
   subject_name VARCHAR(60) NULL,
 PRIMARY KEY (id_subject))
